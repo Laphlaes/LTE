0243a31cb2810632353899de0930d765207f1123
Laphlaes <93842506+Laphlaes@users.noreply.github.com>
2026-09-18 15:16:29 -0300

    Update INSERT statements for professor table
    Replaced old insert statements with new ones for the professor table.

---
+++ INSERT INTO/professor.sql
@@ -0,0 +1,2 @@
+INSERT INTO professor (codigo, nome, codigo_disciplina) VALUES (1, 'Charles Caleiro', 'BD');
+INSERT INTO professor (codigo, nome, codigo_disciplina) VALUES (2, 'Tiago Vieira', 'EN');
